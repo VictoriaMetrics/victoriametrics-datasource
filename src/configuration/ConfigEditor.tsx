fdb3458e6c763665d27aac676018344e2f0677a9
Yury Moladau <yurymolodov@gmail.com>
2022-10-09 19:41:49 +0200

    fix: change default url

--- src/configuration/ConfigEditor.tsx
+++ src/configuration/ConfigEditor.tsx
@@ -3,9 +3,9 @@ import React, { useRef } from 'react';
 import { SIGV4ConnectionConfig } from '@grafana/aws-sdk';
 import { DataSourcePluginOptionsEditorProps, DataSourceSettings } from '@grafana/data';
 import { config } from '@grafana/runtime';
+import {AlertingSettings, DataSourceHttpSettings, Alert, Icon} from '@grafana/ui';
 
 import { getAllAlertmanagerDataSources } from '../../app/features/alerting/unified/utils/alertmanager';
-import { AlertingSettings, DataSourceHttpSettings, Alert } from '../../packages/grafana-ui';
 import { PromOptions } from '../types';
 
 import { AzureAuthSettings } from './AzureAuthSettings';
@@ -35,15 +35,42 @@ export const ConfigEditor = (props: Props) => {
         </Alert>
       )}
 
-      <DataSourceHttpSettings
-        defaultUrl="http://localhost:9090"
-        dataSourceConfig={options}
-        showAccessOptions={showAccessOptions.current}
-        onChange={onOptionsChange}
-        sigV4AuthToggleEnabled={config.sigV4AuthEnabled}
-        azureAuthSettings={azureAuthSettings}
-        renderSigV4Editor={<SIGV4ConnectionConfig {...props}></SIGV4ConnectionConfig>}
-      />
+      <div className="gf-form-group">
+        <h3 className="page-heading">Tips on how to setup</h3>
+
+       <div className="submenu-controls">
+         <div className="gf-form">
+           <a
+             className="gf-form-label gf-form-label--dashlink"
+             href="https://docs.victoriametrics.com/Cluster-VictoriaMetrics.html#url-format"
+             target="_blank"
+             rel="noreferrer">
+             <Icon name="file-alt"/>
+             <span style={{marginLeft: "4px"}}>Cluster VM</span>
+           </a>
+         </div>
+         <div className="gf-form">
+           <a
+             className="gf-form-label gf-form-label--dashlink"
+             href="https://docs.victoriametrics.com/Single-server-VictoriaMetrics.html#grafana-setup"
+             target="_blank"
+             rel="noreferrer">
+             <Icon name="file-alt"/>
+             <span style={{marginLeft: "4px"}}>Grafana setup</span>
+           </a>
+         </div>
+       </div>
+      </div>
+
+        <DataSourceHttpSettings
+          defaultUrl="http://localhost:8428"
+          dataSourceConfig={options}
+          showAccessOptions={showAccessOptions.current}
+          onChange={onOptionsChange}
+          sigV4AuthToggleEnabled={config.sigV4AuthEnabled}
+          azureAuthSettings={azureAuthSettings}
+          renderSigV4Editor={<SIGV4ConnectionConfig {...props}></SIGV4ConnectionConfig>}
+        />
 
       <AlertingSettings<PromOptions>
         alertmanagerDataSources={alertmanagers}
