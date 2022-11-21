3e0ed8b974d2bd10d77c5b95789256a0e04f27c1
dmitryk-dk <kozlovdmitriyy@gmail.com>
2022-11-21 18:04:32 +0200

    {dist,pkg}/plugin: fix step calculation tests

--- pkg/plugin/step_test.go
+++ pkg/plugin/step_test.go
@@ -8,48 +8,57 @@ import (
 )
 
 func Test_calculateStep(t *testing.T) {
-	type timerange struct {
-		from time.Time
-		to   time.Time
-	}
-	type args struct {
+	tests := []struct {
+		name         string
 		baseInterval time.Duration
 		timeRange    backend.TimeRange
 		resolution   int64
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
+		want         string
 	}{
 		{
-			name: "first test",
-			args: args{
-				baseInterval: 20 * time.Second,
-				timeRange: backend.TimeRange{
-					From: time.Now().Add(-time.Hour * 24 * 30),
-					To:   time.Now(),
-				},
-				resolution: 43200,
+			name:         "one month timerange and max point 43200 with 20 second base interval",
+			baseInterval: 20 * time.Second,
+			timeRange: backend.TimeRange{
+				From: time.Now().Add(-time.Hour * 24 * 30),
+				To:   time.Now(),
 			},
-			want: "1m0s",
+			resolution: 43200,
+			want:       "1m0s",
 		},
 		{
-			name: "second test",
-			args: args{
-				baseInterval: 1 * time.Second,
-				timeRange: backend.TimeRange{
-					From: time.Now().Add(-time.Hour * 24 * 30),
-					To:   time.Now(),
-				},
-				resolution: 43200,
+			name:         "one month timerange interval max points 43200 with 1 second base interval",
+			baseInterval: 1 * time.Second,
+			timeRange: backend.TimeRange{
+				From: time.Now().Add(-time.Hour * 24 * 30),
+				To:   time.Now(),
 			},
-			want: "1s",
+			resolution: 43200,
+			want:       "1m0s",
+		},
+		{
+			name:         "one month timerange interval max points 10000 with 5 second base interval",
+			baseInterval: 5 * time.Second,
+			timeRange: backend.TimeRange{
+				From: time.Now().Add(-time.Hour * 24 * 30),
+				To:   time.Now(),
+			},
+			resolution: 10000,
+			want:       "4m19.2s",
+		},
+		{
+			name:         "one month timerange interval max points 10000 with 5 second base interval",
+			baseInterval: 5 * time.Second,
+			timeRange: backend.TimeRange{
+				From: time.Now().Add(-time.Hour * 1),
+				To:   time.Now(),
+			},
+			resolution: 10000,
+			want:       "5s",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := calcStep(tt.args.timeRange, tt.args.baseInterval, tt.args.resolution); got.String() != tt.want {
+			if got := calcStep(tt.baseInterval, tt.timeRange, tt.resolution); got.String() != tt.want {
 				t.Errorf("calculateStep() = %v, want %v", got, tt.want)
 			}
 		})
