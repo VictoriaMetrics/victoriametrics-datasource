82dad9f432db9dfad6def5d032a73471c103a3ec
Yury Moladau <yurymolodov@gmail.com>
2022-12-20 23:18:36 +0100

    fix: add implement required methods

--- src/components/monaco-query-field/getOverrideServices.ts
+++ src/components/monaco-query-field/getOverrideServices.ts
@@ -47,14 +47,40 @@ function makeStorageService() {
   strings.set('expandSuggestionDocs', true.toString());
 
   return {
+    // we do not implement the on* handlers
+    onDidChangeValue: (data: unknown): void => undefined,
+    onDidChangeTarget: (data: unknown): void => undefined,
+    onWillSaveState: (data: unknown): void => undefined,
 
     get: (key: string, scope: unknown, fallbackValue?: string): string | undefined => {
       return strings.get(key) ?? fallbackValue;
     },
 
+    getBoolean: (key: string, scope: unknown, fallbackValue?: boolean): boolean | undefined => {
+      const val = strings.get(key);
+      if (val !== undefined) {
+        // the interface-docs say the value will be converted
+        // to a boolean but do not specify how, so we improvise
+        return val === 'true';
+      } else {
+        return fallbackValue;
+      }
+    },
+
+    getNumber: (key: string, scope: unknown, fallbackValue?: number): number | undefined => {
+      const val = strings.get(key);
+      if (val !== undefined) {
+        return parseInt(val, 10);
+      } else {
+        return fallbackValue;
+      }
+    },
+
     store: (
       key: string,
       value: string | boolean | number | undefined | null,
+      scope: unknown,
+      target: unknown
     ): void => {
       // the interface-docs say if the value is nullish, it should act as delete
       if (value === null || value === undefined) {
@@ -64,15 +90,30 @@ function makeStorageService() {
       }
     },
 
-    remove: (key: string): void => {
+    remove: (key: string, scope: unknown): void => {
       strings.delete(key);
     },
 
-    keys: (): string[] => {
+    keys: (scope: unknown, target: unknown): string[] => {
       return Array.from(strings.keys());
     },
 
-    flush: (): Promise<void> => {
+    logStorage: (): void => {
+      console.log('logStorage: not implemented');
+    },
+
+    migrate: (): Promise<void> => {
+      // we do not implement this
+      return Promise.resolve(undefined);
+    },
+
+    isNew: (scope: unknown): boolean => {
+      // we create a new storage for every session, we do not persist it,
+      // so we return `true`.
+      return true;
+    },
+
+    flush: (reason?: unknown): Promise<void> => {
       // we do not implement this
       return Promise.resolve(undefined);
     },
