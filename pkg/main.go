f2ffb695b9e8201882ddb31c36d7b76ec0a91632
dmitryk-dk <kozlovdmitriyy@gmail.com>
2023-09-28 11:09:56 +0200

    Add version flag, update dependencies

--- pkg/main.go
+++ pkg/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"flag"
 	"os"
 
-	"github.com/VictoriaMetrics/grafana-datasource/pkg/plugin"
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
+
+	"github.com/VictoriaMetrics/grafana-datasource/pkg/buildinfo"
+	"github.com/VictoriaMetrics/grafana-datasource/pkg/plugin"
 )
 
 func main() {
+	flag.Parse()
+	buildinfo.Init()
 	backend.Logger.Info("Starting VictoriaMetrics datasource backend ...")
-	if err := datasource.Manage("victoriametrics-datasource-http-backend", plugin.NewDatasource, datasource.ManageOpts{}); err != nil {
+
+	factory := datasource.InstanceFactoryFunc(plugin.NewDatasource)
+	if err := datasource.Manage("victoriametrics-datasource-http-backend", factory, datasource.ManageOpts{}); err != nil {
 		log.DefaultLogger.Error("Failed to process VictoriaMetrics datasource backend :%s", err.Error())
 		os.Exit(1)
 	}
