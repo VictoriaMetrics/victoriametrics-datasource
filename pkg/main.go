e56e34fb339e962a701e26f3af9dd442c5cc0eec
Dmytro Kozlov <d.kozlov@victoriametrics.com>
2024-07-05 14:26:41 +0200

    Upgrade dependencies (#181)
    * upgrade dependencies before release
    
    * rollback go.sum
    
    * upgrade go version for github actions
    
    * fix golang version
    
    * update golangci-lint
    
    * remove unneeded version, plugin checks it inside
    
    * cleanup

--- pkg/main.go
+++ pkg/main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"flag"
 	"os"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 
-	"github.com/VictoriaMetrics/victoriametrics-datasource/pkg/buildinfo"
 	"github.com/VictoriaMetrics/victoriametrics-datasource/pkg/plugin"
 )
 
 func main() {
-	flag.Parse()
-	buildinfo.Init()
 	backend.Logger.Info("Starting VictoriaMetrics datasource backend ...")
 
 	if err := datasource.Manage("victoriametrics-datasource-http-backend", plugin.NewDatasource, datasource.ManageOpts{}); err != nil {
