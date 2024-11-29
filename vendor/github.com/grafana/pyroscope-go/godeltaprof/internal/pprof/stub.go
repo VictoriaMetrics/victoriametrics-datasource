b913c2c1873f9cdc2f69413145d071ffe5812163
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2024-11-29 15:04:37 +0200

    vendor go dependencies

---
+++ vendor/github.com/grafana/pyroscope-go/godeltaprof/internal/pprof/stub.go
@@ -0,0 +1,14 @@
+package pprof
+
+// unsafe is required for go:linkname
+import _ "unsafe"
+
+//go:linkname runtime_expandFinalInlineFrame runtime/pprof.runtime_expandFinalInlineFrame
+func runtime_expandFinalInlineFrame(stk []uintptr) []uintptr
+
+//go:linkname runtime_cyclesPerSecond runtime/pprof.runtime_cyclesPerSecond
+func runtime_cyclesPerSecond() int64
+
+func Runtime_cyclesPerSecond() int64 {
+	return runtime_cyclesPerSecond()
+}
