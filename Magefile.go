53517b29a38afcf5379ec53848deb47b0378f2ef
Artur Minchukou <aminchukov@victoriametrics.com>
2026-02-11 11:16:50 +0400

    add FreeBSD (amd64) build target for datasource backend (#447)

--- Magefile.go
+++ Magefile.go
@@ -4,25 +4,28 @@
 package main
 
 import (
-        // mage:import
-        "github.com/grafana/grafana-plugin-sdk-go/build"
-        "github.com/magefile/mage/mg"
+	// mage:import
+	"github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/mg"
 )
 
 func init() {
-        build.SetBeforeBuildCallback(func(cfg build.Config) (build.Config, error) {
-                // Do something before building
-                cfg.OutputBinaryPath = "plugins/victoriametrics-metrics-datasource"
-                return cfg, nil
-        })
+	build.SetBeforeBuildCallback(func(cfg build.Config) (build.Config, error) {
+		// Do something before building
+		cfg.OutputBinaryPath = "plugins/victoriametrics-metrics-datasource"
+		return cfg, nil
+	})
 }
 
 func Build() {
-        b := build.Build{}
-        linuxS390 := func () error {
-                return b.Custom("linux", "s390x")
-        }
-        mg.Deps(b.Linux, b.Windows, b.Darwin, b.DarwinARM64, b.LinuxARM64, b.LinuxARM, linuxS390)
+	b := build.Build{}
+	linuxS390 := func() error {
+		return b.Custom("linux", "s390x")
+	}
+	freebsd := func() error {
+		return b.Custom("freebsd", "amd64")
+	}
+	mg.Deps(b.Linux, b.Windows, b.Darwin, b.DarwinARM64, b.LinuxARM64, b.LinuxARM, linuxS390, freebsd)
 }
 
 // Default configures the default target.
