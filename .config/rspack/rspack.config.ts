ef592787fe0ecc52c5ca165a03e201a065eccbd4
Artur Minchukov <aminchukov@victoriametrics.com>
2026-09-09 13:01:23 +0400

    chore(config): upgrade version to 7.10.0 and improve TerserPlugin configuration
    - Updated `.cprc.json` to version 7.10.0.
    - Enhanced TerserPlugin to set `LICENSE.txt` as the output file for extracted comments in both Webpack and Rspack configurations.

--- .config/rspack/rspack.config.ts
+++ .config/rspack/rspack.config.ts
@@ -117,6 +117,11 @@ const config = async (env): Promise<Configuration> => {
       minimize: Boolean(env.production),
       minimizer: [
         new TerserPlugin({
+          extractComments: {
+            banner: false,
+            filename: 'LICENSE.txt',
+          },
+
           terserOptions: {
             format: {
               comments: (_, { type, value }) => type === 'comment2' && value.trim().startsWith('[create-plugin]'),
@@ -193,7 +198,7 @@ const config = async (env): Promise<Configuration> => {
         },
       ]),
       new SubresourceIntegrityPlugin({
-        hashFuncNames: ["sha256"],
+        hashFuncNames: ['sha256'],
       }),
       ...(env.development
         ? [
